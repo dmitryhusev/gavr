534a0a3acff375c637256b1ec35e61b69b16bf7f
dmitryhusev <dimtri.gusev@gmail.com>
2023-09-23 03:32:41 +0200

    .

--- gavr/settings.py
+++ gavr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DO = os.getenv('ALLOWED_HOST')
+DO = os.getenv('ALLOWED_HOST', '')
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', DO]
 
 
