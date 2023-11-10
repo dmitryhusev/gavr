a9d7064c458355168259a3007dad3cec84702414
dmitry <dimtri.gusev@gmail.com>
2023-11-10 01:01:16 +0100

    fix chrome options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 from .utils.main import Page
 
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
 @pytest.fixture
 def action():
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=chrome_options)
     browser.maximize_window()
     yield Page(browser)
     browser.quit()
