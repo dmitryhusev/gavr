91d41600739c9b29bc98a3802d0069afb99ba09e
dmitry <dimtri.gusev@gmail.com>
2023-11-10 01:15:53 +0100

    add attachment

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
 from .utils.main import Page
+import allure
+from allure_commons.types import AttachmentType
 
+
+def make_attachment(browser):
+    allure.attach(browser.get_screenshot_as_png(), 'screenshot', AttachmentType.PNG)
+    allure.attach(browser.current_url, 'url', AttachmentType.TEXT)
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -12,4 +18,5 @@ def action():
     browser = webdriver.Chrome(options=chrome_options)
     browser.maximize_window()
     yield Page(browser)
+    make_attachment(browser)
     browser.quit()
