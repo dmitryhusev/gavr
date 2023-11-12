5f928f461a022c152f2eb629537eee4e3c63952e
dmitryhusev <dimtri.gusev@gmail.com>
2023-11-12 20:48:34 +0100

    added docker stuff

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10-alpine
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app/
+
+COPY requirements.txt /app/
+
+RUN pip install --upgrade pip && pip install -r requirements.txt
--- gavr/settings.py
+++ gavr/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv('DEBUG', False))
+DEBUG = int(os.getenv('DEBUG'))
 ALLOWED_HOSTS = (os.getenv('DJANGO_ALLOWED_HOSTS').split(' '))
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # custom apps
     'accounts',
     'posts',
+    
 ]
 
 MIDDLEWARE = [
