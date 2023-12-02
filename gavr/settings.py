a9827c0f030ad0058a429b926a34e38c5a3cc229
dmitryhusev <dimtri.gusev@gmail.com>
2023-12-02 01:59:02 +0100

    deploy to cloud 1

--- gavr/settings.py
+++ gavr/settings.py
@@ -173,4 +173,4 @@ ACCOUNT_FORMS = {
 'signup': 'accounts.forms.CustomSignupForm',
 }
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://161.35.208.55', 'https://gav-rik.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://206.81.26.36', 'https://gav-rik.com']
