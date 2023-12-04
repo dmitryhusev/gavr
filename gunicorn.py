ad8bd293dd0d6de579a96ff18e31044f67a37470
dmitryhusev <dimtri.gusev@gmail.com>
2023-12-04 03:11:20 +0100

    deploy to cloud 8

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,6 @@
-wsgi_app = "gavr.wsgi"
+wsgi_app = "gavr.wsgi:application"
 loglevel = "debug"
 workers = 2
 bind = "0.0.0.0:8000"
 accesslog = errorlog = "/var/log/gunicorn/dev.log"
 capture_output = True
-daemon = True
